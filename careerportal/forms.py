28e6837475b04db87ac48ebea1ea2a94765c7b6d
Your Name <your@email.com>
2026-04-14 00:36:11 +0200

    updated forms.py

--- careerportal/forms.py
+++ careerportal/forms.py
@@ -1,13 +1,27 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from django.contrib.auth.models import User
+from .models import Company
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    USER_TYPE_CHOICES = (
+        ('applicant', 'Applicant'),
+        ('employee', 'Employee'),)
+
+    user_type = forms.ChoiceField(choices=USER_TYPE_CHOICES)
+
+    # Applicant fields
+    bio = forms.CharField(required=False)
+    summary = forms.CharField(required=False)
+
+    # Employee field
+    company = forms.ModelChoiceField(
+        queryset=Company.objects.all(),
+        required=False)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'role', 'password1', 'password2']
+        fields = ['username', 'password1', 'password2']
     
     # - Meta defines how the form maps to the database model.
     # - Email is included to support user identification and communication.
