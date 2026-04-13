9e2a94fb100aa054ed59d1a7f3681dcc2c1019f6
Your Name <your@email.com>
2026-04-14 01:20:51 +0200

    updating url

--- careerportal/urls.py
+++ careerportal/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('profile/', views.profile_view, name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='careerportal/login.html'),name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),]
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),]
