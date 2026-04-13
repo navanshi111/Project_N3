aee7ab3908b9ed89c995374d4aeac2ddf24f0e1b
Your Name <your@email.com>
2026-04-14 00:32:11 +0200

    removed AUTH_USER_MODEL

--- website/settings.py
+++ website/settings.py
@@ -40,8 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 ]
 
-AUTH_USER_MODEL = 'careerportal.User'
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
