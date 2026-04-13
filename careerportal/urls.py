7708dc076f41f09d6879c27b004b5274ab2c7dfe
Your Name <your@email.com>
2026-04-14 00:09:03 +0200

    adding urls to the views

--- careerportal/urls.py
+++ careerportal/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
-from .views import register_view, profile_view, accounts_home
-from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', accounts_home, name='accounts_home'),
-    path('register/', register_view, name='register'),
-    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('profile/', profile_view, name='profile'),]
+    # Home
+    path('', views.home, name='home'),
+
+    # Register
+    path('register/', views.register_view, name='register'),
+
+    # Profile
+    path('profile/', views.profile_view, name='profile'),
+
+    # Login/Logout
+    path(
+        'login/',
+        auth_views.LoginView.as_view(
+            template_name='careerportal/login.html'
+        ),
+        name='login'
+    ),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),]
--- careerportal/views.py
+++ careerportal/views.py
@@ -2,25 +2,43 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm
-
-# Source: https://docs.djangoproject.com/en/5.2/intro/tutorial03/
-# Source: Lecture 5 (Properties)
+from .models import Applicant, Employee
 
 def register_view(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+
+            user_type = form.cleaned_data.get("user_type")
+
+            if user_type == "applicant":
+                Applicant.objects.create_user(
+                    username=form.cleaned_data["username"],
+                    password=form.cleaned_data["password1"],
+                    email=form.cleaned_data.get("email"),
+                    bio=form.cleaned_data["bio"],
+                    summary=form.cleaned_data["summary"],)
+
+            elif user_type == "employee":
+                Employee.objects.create_user(
+                    username=form.cleaned_data["username"],
+                    password=form.cleaned_data["password1"],
+                    email=form.cleaned_data.get("email"),
+                    company=form.cleaned_data["company"],)
+
             messages.success(request, "Account created successfully")
             return redirect('login')
     else:
         form = RegisterForm()
 
-    return render(request, 'accounts/register.html', {'form': form})
+    return render(request, 'careerportal/register.html', {'form': form})
 
 @login_required
 def profile_view(request):
-    return render(request, 'accounts/profile.html')
+    return render(request, 'careerportal/profile.html')
 
-def accounts_home(request):
-    return render(request, 'accounts/home.html')
+def home(request):
+    return render(request, 'careerportal/home.html')
+
+# Source: https://docs.djangoproject.com/en/5.2/intro/tutorial03/
+# Source: Lecture 5 (Properties)
