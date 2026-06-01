bc6f941985ba5b34b0098d54a20bc5716fcd673a
Neha <neha.nagabhirava@gmail.com>
2026-06-01 15:54:40 +0200

    Add application status tracking and admin improvements

--- careerportal/admin.py
+++ careerportal/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Applicant, Employee, Company, Job, Application, SavedJob
+from .models import Company, Job, Applicant, Employee, Application, SavedJob
 
 # Company
-admin.site.register(Company)
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
 
 #Job
-admin.site.register(Job)
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_by', 'job_type', 'required_degree', 'deadline')
+    list_filter = ('job_type', 'required_degree', 'pay_status')
+    search_fields = ('title', 'description')
 
 # Applicant Admin
 @admin.register(Applicant)
@@ -23,7 +29,8 @@ class EmployeeAdmin(UserAdmin):
  # Application Admin
 @admin.register(Application)
 class ApplicationAdmin(admin.ModelAdmin):
-    list_display = ['applicant', 'job', 'status', 'created_at']
+    list_display = ('applicant', 'job', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
 
 # Saved Job Admin
 @admin.register(SavedJob)
